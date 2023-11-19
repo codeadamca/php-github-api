60660585e19e2ca0f761ec64120d9951d4e314e3
Adam Thomas <thomasadam83@hotmail.com>
2023-11-19 14:48:20 -0500

    Added first API sample

---
+++ organization-repos.php
@@ -0,0 +1,46 @@
+<?php
+
+$env = file(__DIR__.'/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+foreach($env as $value)
+{
+  $value = explode('=', $value);  
+  define($value[0], $value[1]);
+}
+
+$url = 'https://api.github.com/orgs/BrickMMO/repos';
+
+$headers[] = 'Content-type: application/json';
+$headers[] = 'Authorization: Bearer '.GITHUB_ACCESS_TOKEN;
+$headers[] = 'User-Agent: Awesome-Octocat-App   ';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+$result = json_decode($result, true);
+
+// echo '<pre>';
+// print_r($result);
+// echo '</pre>';
+
+foreach($result as $key => $repo)
+{
+  echo '<h1>'.$repo['name'].'</h1>';
+  echo '<ul>
+      <li>ID: '.$repo['id'].'</li>
+      <li>Full Name: '.$repo['full_name'].'</li>
+      <li>Owner: '.$repo['owner']['login'].'</li>
+      <li>URL: <a href="'.$repo['html_url'].'">'.$repo['html_url'].'</a></li>
+      <li>Created At: '.date_format(date_create($repo['created_at']), "F jS, Y g:i a").'</li>
+      <li>Updated At: '.date_format(date_create($repo['updated_at']), "F jS, Y g:i a").'</li>
+      <li>Pushed At: '.date_format(date_create($repo['pushed_at']), "F jS, Y g:i a").'</li>
+    </ul>';
+}
